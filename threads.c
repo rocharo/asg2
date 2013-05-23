430bc2a9f22e4cc402a2abc33b04a3540476be18
bkeyoumarsi <bkeyouma@ucsc.edu>
2013-05-22 22:34:05 -0700

    added some more details to the output

--- threads.c
+++ threads.c
@@ -1,8 +1,12 @@
-//
-//  threads.c
-//  
-//
-//
+/*
+ * Bardia Keyoumarsi
+ * Vincent Lantaca
+ * Alexis Rocha
+ *
+ * CMPS 111
+ * Assignement 2 - Thread scheduler
+ *
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,17 +18,18 @@
 #include <ucontext.h>
 
 #define NUMBER_OF_THREADS 10
-#define NUMBER_OF_TICKETS 1000
+#define NUMBER_OF_TICKETS 100
 
 static ucontext_t ctx[NUMBER_OF_THREADS];
 
 static int tickets[NUMBER_OF_TICKETS];
+static int threadCount[NUMBER_OF_THREADS] = {0};
 
 static void test_thread(void);
 static int thread = 0;
-void thread_exit(int);
+static int ticketCount = 0;
 
-int ticketCount = 0;
+void thread_exit(int);
 
 static struct sigaction sa; 
 static struct itimerval timer;
@@ -33,18 +38,17 @@ static struct itimerval timer;
 void thread_yield(int signum) {
     int old_thread = thread;
     
-    // This is the scheduler, it is a bit primitive right now
-    //thread = 1+thread;
+    //The scheduler picks a ticket from the array and swaps the threads
 	thread = tickets[ticketCount++];
 
-    printf("Thread %d yielding to thread %d\n", old_thread, thread);
-    printf("Thread %d calling swapcontext\n", old_thread);
+    //printf("Thread %d yielding to thread %d\n", old_thread, thread);
+    //printf("Thread %d calling swapcontext\n", old_thread);
     
     // This will stop us from running and restart the other thread
     swapcontext(&ctx[old_thread], &ctx[thread]);
 
     // The other thread yielded back to us
-    printf("Thread %d back in thread_yield\n", thread);
+    //printf("Thread %d back in thread_yield\n", thread);
 }
 
 // This is the main thread
@@ -56,17 +60,18 @@ int main(void) {
     
     printf("Main calling thread_create\n");
 
-	// Creat 10 threads
+	// Create 10 threads
 	int i;
-	for (i = 0; i < 10; i++){
+	for (i = 0; i < NUMBER_OF_THREADS; i++){
 		thread_create(&test_thread);
 		thread++;
 	}
 	thread = 0;
 	
 	// Assign tickets to threads
-	for (i=0; i < 1000; i++){
-		tickets[i] = (rand() % 10);
+	for (i=0; i < NUMBER_OF_TICKETS; i++){
+		tickets[i] = (rand() % NUMBER_OF_THREADS);
+		threadCount[tickets[i]]++;
 	}
     
     printf("Main returned from thread_create\n"); 
@@ -89,7 +94,11 @@ int main(void) {
 	printf("Main returned from create_timer\n");
 
     // Loop, doing a little work then yielding to the other thread
-    while(ticketCount<1000);
+    while(ticketCount<100);
+	
+	for (i = 0; i < NUMBER_OF_THREADS; i++)
+		printf("Thread %d was allowed to run %d times.\n",
+		       i, threadCount[i]);
 
     // We should never get here
     exit(0);
@@ -100,16 +109,9 @@ int main(void) {
 static void test_thread(void) {
     printf("In test_thread\n");
 
-    // Loop, doing a little work then yielding to the other thread
-    while(1) {
-        
-        //printf("Test_thread just doing nothing\n");
-        
-        //thread_yield();
-        
-        //printf("Test_thread returned from thread_yield\n");
-    }
-    printf("Exiting thread...\n");
+    // doing busy work
+    while(1);
+	
     thread_exit(0);
 }
 
